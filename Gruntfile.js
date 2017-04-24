dd400462af330213cd011be5639bfa8adb77297d
corey <csundahl@gmail.com>
2017-04-24 12:15:40 -0400

    change bower dependency destination

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,20 @@ module.exports = function(grunt) {
 
   require('jit-grunt')(grunt);
 
+  // Unified Watch Object
+	var watchFiles = {
+		serverViews: ['app/views/**/*.*'],
+		serverJS: ['gruntfile.js', 'app.js', 'config/**/*.js', 'app/**/*.js', '!app/tests/'],
+
+		clientViews: ['public/modules/**/views/**.html'],
+		clientJS: ['public/js/*.js', 'public/form_modules/**/*.js', 'public/modules/**/*.js'],
+		clientCSS: ['public/modules/**/*.css', 'public/form_modules/**/*.css', '!public/modules/**/demo/**/*.css', '!public/modules/**/dist/**/*.css'],
+
+		serverTests: ['app/tests/**/*.js'],
+		clientTests: ['public/modules/**/tests/*.js', '!public/modules/**/dist/**/*.js', '!public/modules/**/node_modules/**/*.js']
+	};
+
+	watchFiles.allTests = watchFiles.serverTests.concat(watchFiles.clientTests);
   // Project configuration.
   grunt.initConfig({
 
