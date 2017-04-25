c98b9fd0cc9fe25c4f1a7381dce28560b828b86f
corey <csundahl@gmail.com>
2017-04-25 14:47:43 -0400

    hard coding title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Starter App' });
+  res.render('index');
 });
 
 router.post('/login', passport.authenticate('local', {
