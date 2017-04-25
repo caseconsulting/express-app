0040eb08fa92b0d6ecea890bcf1e06486cb0d353
corey <csundahl@gmail.com>
2017-04-25 13:56:07 -0400

    added environment feedback

--- app.js
+++ app.js
@@ -86,3 +86,11 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// Logging initialization
+console.log('--');
+console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
+console.log(chalk.green('Port:\t\t\t\t' + config.port));
+console.log(chalk.green('Database:\t\t\t' + config.db.uri));
+console.log('--');
+
