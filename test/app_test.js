be1f205f3751032ec112a313a31b618e0b2c555c
corey <csundahl@gmail.com>
2017-05-01 08:22:13 -0400

    updating tests

--- test/app_test.js
+++ test/app_test.js
@@ -40,3 +40,31 @@ describe('GET /signup', function () {
   });
 });
 
+/**
+ * Test Error Routes
+ */
+
+describe('GET /404', function () {
+  it('should return 404', function (done) {
+    request(app)
+      .get('/404')
+      .expect(404, done);
+  });
+});
+
+describe('GET /403', function () {
+  it('should return 403', function (done) {
+    request(app)
+      .get('/403')
+      .expect(403, done);
+  });
+});
+
+describe('GET /500', function () {
+  it('should return 500', function (done) {
+    request(app)
+      .get('/500')
+      .expect(500, done);
+  });
+});
+
